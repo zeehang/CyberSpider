39e9ad617e419f05132a32588ec6421ca98694a0
zeehang <myque@live.com>
2016-03-08 18:51:19 -0800

    Initial Commit

---
+++ CyberSpider/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CyberSpider
+//
+//  Created by Michael Zhang on 3/8/16.
+//  Copyright © 2016 Michael Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
